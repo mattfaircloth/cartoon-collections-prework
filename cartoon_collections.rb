f7306c9c5942af32e34b6cc354ede259132db761
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-25 16:19:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ def find_the_cheese(foods)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  if foods.find do |food|
+  if foods.select do |food|
     food == "cheddar" || food == "gouda" || food == "camembert"
     return food
     end
